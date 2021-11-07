925498c3790358a170b716949ba0cd32f4d1df13
Kathrin Thenhausen <kathrin.thenhausen@student.hpi.de>
2021-11-07 14:56:13 +0100

    show authors on papers detail page

--- spec/features/paper/show_paper_spec.rb
+++ spec/features/paper/show_paper_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-describe "Paoer show page test" do
+describe "Paper show page test" do
     it "should visit an papers details page" do
         @alan = Paper.new(:title => "alan", :venue => "turing", :year => "1912")
         @alan.save
@@ -8,5 +8,15 @@ describe "Paoer show page test" do
         expect(page).to have_text("alan")
         expect(page).to have_text("turing")
         expect(page).to have_text("1912")
+    end
+
+    it "should show associated authors" do
+        @alan = Paper.new(:title => "alanspaper", :venue => "GB", :year => "1912")
+        @alan.save
+        @writer = Author.new(:first_name => "alan", :last_name => "turing", :homepage => "http://wikipedia.org/Alan_Turing")
+        @alan.authors << @writer
+        visit paper_path(@alan)
+        expect(page).to have_text("alan")
+        expect(page).to have_text("turing")
     end
 end
