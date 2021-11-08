b1048a8cb01a44b3dea50babec85f6e0e6fe822a
Kathrin Thenhausen <kathrin.thenhausen@student.hpi.de>
2021-11-08 19:37:07 +0100

    tests, assigning on edit page

--- spec/features/paper/edit_paper_spec.rb
+++ spec/features/paper/edit_paper_spec.rb
@@ -14,8 +14,27 @@ describe "Edit page", type: :feature do
         @ff.save
         visit edit_paper_path(@ff)
         page.fill_in 'paper[title]', with: "derHund"
-        find('input[type="submit"]').click
+        find('input[id="submit-edit"]').click
         @ff.reload
         expect(@ff.title).to eq("derHund")
+    end
+end
+
+describe "Assign authors to paper test" do
+    it "should assign chosen authors to paper" do
+        @writer = Author.new(:first_name => "alan", :last_name => "turing", :homepage => "http://wikipedia.org/Alan_Turing")
+        @writer.save
+        x=@writer[:ids]
+        @alan = Paper.new(:title => "alanspaper", :venue => "GB", :year => "1912")
+        @alan.save
+        visit edit_paper_path(@alan)
+        expect(page).to have_text("Select authors of the paper:")
+
+        # Unable to find option nil within #<Capybara::Node::Element tag="select" path="/html/body/form/select">
+        select value=x, from: "paper[author_ids][]"
+        find('input[id="submit-authors"]').click
+        expect(@alan.authors).to eq([@writer])
+        #expect(x).to be("1") #
+        #expect(Author.where(first_name: "alan")[:ids]).to be("1")
     end
 end
--- spec/features/paper/show_paper_spec.rb
+++ spec/features/paper/show_paper_spec.rb
@@ -20,20 +20,3 @@ describe "Paper show page test" do
         expect(page).to have_text("turing")
     end
 end
-
-describe "Assign authors to paper test" do
-    it "should assign chosen authors to paper" do
-        @alan = Paper.new(:title => "alanspaper", :venue => "GB", :year => "1912")
-        @alan.save
-        visit paper_path(@alan)
-        expect(page).to have_text("Select authors of the paper:")
-        @writer = Author.new(:first_name => "alan", :last_name => "turing", :homepage => "http://wikipedia.org/Alan_Turing")
-        @writer.save
-        # x=writer.author_ids
-        # Unable to find option nil within #<Capybara::Node::Element tag="select" path="/html/body/form/select">
-        # select value=x, from: "paper[author_ids][]"
-
-        #expect(x).to be("1") #
-        #expect(Author.where(first_name: "alan")[:ids]).to be("1")
-    end
-end
